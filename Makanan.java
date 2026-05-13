ae578b95010d6af4403739be76a3c8abde7d0f21
qiqifahreza <bungbeng002@gmail.com>
2026-05-13 10:21:02 +0700

    Implement Makanan class extending Produk with kadaluarsa attribute and methods for hitungAkhir and tampilInfo

--- Makanan.java
+++ Makanan.java
@@ -1,3 +1,20 @@
-public class Makanan {
-    
+public class Makanan extends Produk {
+    private int kadaluarsa;
+
+    public Makanan(String nama, double harga, int stok, int kadaluarsa) {
+        super(nama, harga, stok);
+        this.kadaluarsa = kadaluarsa;
+    }
+
+    @Override
+    public double hitungAkhir(int jmlhBeli) {
+        System.out.println("Total harga = " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
+
+    @Override 
+    public void tampilInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Kadaluarsa: " + kadaluarsa + " Tahun");
+    }
 }
