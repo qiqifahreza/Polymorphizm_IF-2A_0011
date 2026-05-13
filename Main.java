a0bc9f2c1ab20ff78c4434a4cf90d058586b8f29
qiqifahreza <bungbeng002@gmail.com>
2026-05-13 11:22:25 +0700

    Implement main method to initialize Produk instances and Kasir

--- Main.java
+++ Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String [] args) {
+        Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
+        Produk p2 = new Makanan("Bakso", 1000, 100, 3);
+        Kasir k = new Kasir("Maman");
+
+        
+
+    }
 }
