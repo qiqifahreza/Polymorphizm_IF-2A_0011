caeda0cb2322bfb2dd3b1d5b774052030dd23ee3
qiqifahreza <bungbeng002@gmail.com>
2026-05-13 12:05:15 +0700

    Rename variable 'k' to 'kasir' for improved clarity in main method

--- Main.java
+++ Main.java
@@ -2,19 +2,19 @@ public class Main {
     public static void main(String [] args) {
         Produk p1 = new Elektronik("Samsung", 100000, 5, 2);
         Produk p2 = new Makanan("Bakso", 1000, 100, 3);
-        Kasir k = new Kasir("Maman");
+        Kasir kasir = new Kasir("Maman");
 
         p1.tampilInfo();
 
-        k.tampilIdentitas();
-        k.prosesBelanja(p1, 3);
+        kasir.tampilIdentitas();
+        kasir.prosesBelanja(p1, 3);
 
         System.out.println("\n\n");
 
         p2.tampilInfo();
 
-        k.tampilIdentitas();
-        k.prosesBelanja(p2, 11);
+        kasir.tampilIdentitas();
+        kasir.prosesBelanja(p2, 11);
 
     }
 }
