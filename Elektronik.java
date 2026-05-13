f06af161839112b5d962faca93ce16f6d6707f29
qiqifahreza <bungbeng002@gmail.com>
2026-05-13 10:14:02 +0700

    Implement Elektronik class extending Produk with masaGaransi and methods for hitungAkhir and tampilInfo

--- Elektronik.java
+++ Elektronik.java
@@ -1,3 +1,20 @@
-public class Elektronik {
-    
+public class Elektronik extends Produk {
+    private int masaGaransi;
+
+    public Elektronik(String nama, double harga, int stok, int masaGaransi) {
+        super(nama, harga, stok);
+        this.masaGaransi = masaGaransi;
+    }
+
+    @Override
+    public double hitungAkhir(int jmlhBeli) {
+        System.out.println("Total harga = " + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
+
+    @Override 
+    public void tamplInfo() {
+        super.tampilInfo();
+        System.out.println("Masa Garansi: " + masaGaransi + " Tahun");
+    }
 }
