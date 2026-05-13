803e18d4552a17ba8849836b47e4deca37f28d84
qiqifahreza <bungbeng002@gmail.com>
2026-05-13 09:41:56 +0700

    Add initial class definitions for Elektronik, Kasir, Main, Makanan, and Produk

---
+++ Produk.java
@@ -0,0 +1,20 @@
+public abstract class Produk {
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    
+
+
+
+
+
+
+
+}
